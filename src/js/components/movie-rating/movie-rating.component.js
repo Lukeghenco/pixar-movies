c305b7981342a6c43d646b9a849fc97a595996b2
Lukeghenco <lukeghenco@gmail.com>
2016-06-22 21:46:10 -0700

    Added basic express.js server.

--- src/js/components/movie-rating/movie-rating.component.js
+++ src/js/components/movie-rating/movie-rating.component.js
@@ -1,23 +1,34 @@
 'use strict';
 
+function buildEntries(value, max) {
+  var entries = [];
+
+  for (var i = 1; i <= max; i++) {
+    var icon = (i <= value ? "glyphicon-star" : "glyphicon-star-empty");
+    entries.push(icon)
+  }
+
+  return entries;
+}
+
 function MovieRatingController() {
 
   var ctrl = this;
 
   ctrl.$onInit = function() {
-    ctrl.entries = new Array(ctrl.value);
+    ctrl.entries = buildEntries(ctrl.value, ctrl.max);
   };
 
   ctrl.$onChanges = function() {
-    ctrl.entries = new Array(ctrl.value);
+    ctrl.entries = new Array(ctrl.max);
   };
 }
 
 app.component('movieRating', {
   templateUrl: "/src/js/components/movie-rating/movie-rating.component.html",
   bindings: {
-    value: "<" // input
+    value: "<", //  "<" == input
+    max: "<"
   },
-  transclude: true,
   controller: MovieRatingController
 });
